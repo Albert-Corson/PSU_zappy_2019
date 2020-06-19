9f68e7eaa4198a873eb6a3ec1b5aaa3cf3b8dacd
F <fahad.assoumani@epitech.eu>
2020-06-19 21:39:48 +0200

    [ADD] requirements.txt with tests, freeze depencies and update ai_build rule to install deps in ci

--- ai.mk
+++ ai.mk
@@ -1,5 +1,5 @@
 include var.mk sources.mk
 
 $(NAME_AI):
-	cd ai ; python3 setup.py build
+	cd ai ; pip install -r requirements.txt ; python3 setup.py build
 	cp ai/build/exe.linux-x86_64-3.8/main ./zappy_ai
